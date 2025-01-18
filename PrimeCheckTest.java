e5ca997b770abba6d1e83a808d075755b1551cea
No Names <vladmak04@gmail.com>
2025-01-18 01:58:56 +0100

    fix typo

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -33,6 +33,6 @@ public class PrimeCheckTest {
   public void testNonPrimes() {
     assertEquals("6 is not a prime", false, PrimeCheck.isPrime(6));
     assertEquals("12 is not a prime", false, PrimeCheck.isPrime(12));
-    assertEquals("21 is not a prime", true, PrimeCheck.isPrime(21));
+    assertEquals("21 is not a prime", false, PrimeCheck.isPrime(21));
   }
 }
